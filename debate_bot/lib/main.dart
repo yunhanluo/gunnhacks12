9732a0465d0e3cdd06727f629147e69c8c4ffbda
Bloob <acceleration9992@gmail.com>
2026-02-21 15:44:00 -0800

    openai stuff

--- debate_bot/lib/main.dart
+++ debate_bot/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'home.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
   runApp(const MyApp());
 }
 
---
+++ debate_bot/lib/services/openai_service.dart
@@ -0,0 +1,39 @@
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:convert';
+
+class OpenAIService {
+  static final String _apiKey = dotenv.env["OPENAI_API_KEY"] ?? 'fallback';
+
+  static Future<dynamic> askAI({
+    String? systemMessage,
+    String? userMessage,
+  }) async {
+    final evalResponse = await http.post(
+      Uri.parse('https://api.openai.com/v1/chat/completions'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $_apiKey',
+      },
+      body: jsonEncode({
+        'model': 'gpt-3.5-turbo',
+        'messages':
+            (systemMessage == null
+                ? []
+                : [
+                    {'role': 'system', 'content': systemMessage},
+                  ]) +
+            (userMessage == null
+                ? []
+                : [
+                    {'role': 'user', 'content': userMessage},
+                  ]),
+        'max_tokens': 500,
+        'temperature': 0.7,
+        'response_format': {'type': 'json_object'},
+      }),
+    );
+
+    return jsonDecode(evalResponse.body);
+  }
+}
