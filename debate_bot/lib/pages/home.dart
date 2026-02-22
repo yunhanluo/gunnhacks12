2721535fc597ae5a359e6d4433a6d10dfb5fb773
Brandon Zhang <bb32@Brandons-MacBook-Pro.local>
2026-02-22 10:14:10 -0800

    same height aff/neg switch

--- debate_bot/lib/pages/home.dart
+++ debate_bot/lib/pages/home.dart
@@ -155,7 +155,7 @@ class _HomeState extends State<Home> {
     );
   }
 
-  Widget _buildSegmentedButton() {
+  Widget _buildSegmentedButton(bool isNarrow) {
     return SegmentedButton<Side>(
       showSelectedIcon: false,
       style: ButtonStyle(
@@ -163,8 +163,16 @@ class _HomeState extends State<Home> {
           RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
         ),
       ),
-      segments: const <ButtonSegment<Side>>[
-        ButtonSegment<Side>(value: Side.aff, label: Text('AFF')),
+      segments: <ButtonSegment<Side>>[
+        ButtonSegment<Side>(
+          value: Side.aff,
+          label: Padding(
+            padding: !isNarrow
+                ? EdgeInsets.symmetric(vertical: 14)
+                : EdgeInsets.all(0),
+            child: Text('AFF'),
+          ),
+        ),
         ButtonSegment<Side>(value: Side.neg, label: Text('NEG')),
       ],
       selected: <Side>{sideView},
@@ -222,7 +230,7 @@ class _HomeState extends State<Home> {
                   child: isNarrow
                       ? Column(
                           children: [
-                            _buildSegmentedButton(),
+                            _buildSegmentedButton(isNarrow),
                             SizedBox(height: 8),
                             Row(
                               children: [
@@ -247,7 +255,7 @@ class _HomeState extends State<Home> {
                         )
                       : Row(
                           children: [
-                            _buildSegmentedButton(),
+                            _buildSegmentedButton(isNarrow),
                             SizedBox(width: 8),
                             Expanded(
                               child: TextField(
